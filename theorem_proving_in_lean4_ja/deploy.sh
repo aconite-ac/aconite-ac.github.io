65baa5ad4f4dba56b73359e17c413f86c85dd52b
aconite-ac <aconitefield@gmail.com>
2023-07-30 01:57:07 +0900

    ``introduction.md`` の訳を微修正

--- theorem_proving_in_lean4_ja/deploy.sh
+++ theorem_proving_in_lean4_ja/deploy.sh
@@ -13,6 +13,6 @@ git init
 cp -r ../out/./ .
 git add -A
 git commit -m "Update `date`"
-git push https://github.com/aconite-ac/theorem_proving_in_lean4.git +HEAD:gh-pages
+git push git@github.com:aconite-ac/theorem_proving_in_lean4.git +HEAD:gh-pages
 cd ..
 rm -rf deploy
